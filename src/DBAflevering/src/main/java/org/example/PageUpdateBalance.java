c43816b3a3d1ec1c2ad9f90a4b92e23a7a91b260
MalteFP <MalteFp@outlook.com>
2026-01-16 14:15:11 +0100

    Fixed missing file

---
+++ src/DBAflevering/src/main/java/org/example/PageUpdateBalance.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PageUpdateBalance extends Page {
+
+    @Override
+    protected void display() throws SQLException {
+        Statement getProfile = connection.createStatement();
+        ResultSet profile = getProfile.executeQuery("SELECT * FROM Customers");
+        System.out.println("Balance: " + profile.getString("Balance"));
+        System.out.println("Do you want to add/withdraw from your balance?");
+
+    }
+
+    @Override
+    protected Page nextPage() {
+        return new PageProfile().init(connection);
+    }
+
+    @Override
+    protected void act() throws SQLException {
+        Statement addToBalance = connection.createStatement();
+        addToBalance.executeUpdate("UPDATE Customers SET Balance = Balance + " + decision());
+    }
+
+    private int decision() {
+        return Utils.reader(-10000, 10000);
+    }
+}
