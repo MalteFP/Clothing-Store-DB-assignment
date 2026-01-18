b14775bdfb5b9379ea2f074920ef18042b92dcef
Tobias Staunsholm <Tobias2017@iexcel.dk>
2026-01-18 20:05:53 +0100

    Add new customers created

--- src/DBAflevering/src/main/java/org/example/PageAddNewCustomer.java
+++ src/DBAflevering/src/main/java/org/example/PageAddNewCustomer.java
@@ -1,20 +1,46 @@
 package org.example;
 
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
-public class PageAddNewCustomer extends Page {
+public class PageAddNewCustomer extends Page{
     @Override
     protected void display() throws SQLException {
-
+        //Formatting for split
+        System.out.println("Please enter the relevant customer information in the following format: ");
+        System.out.println("FullName, Adress, ZipCode, City, Balance");
     }
 
     @Override
     protected Page nextPage() {
-        return null;
+        return new PageMainMenuEmployee().init(connection);
     }
 
     @Override
     protected void act() throws SQLException {
+        //Split for database
+        boolean correctFormat = true;
+        String[] columns = decision().split(", "); //ChatGPT told us about split
+        try {
+            int tester = Integer.parseInt(columns[2]);
+            tester = Integer.parseInt(columns[4]);
+        }catch (Exception e){
+            System.out.println("Formating error");
+            correctFormat = false;
+        }
+        //Adds customer to database
+        if(correctFormat){
+            Statement addItem = connection.createStatement();
+            addItem.executeUpdate(
+                    "INSERT INTO Customers (FullName, Adress, ZipCode, City, Balance) VALUES ('" + columns[0] + "', '" + columns[1] + "', '" + columns[2] + "', " + columns[3] + " , " + columns[4] + ")"
+            );
 
+        }
+    }
+
+    private String decision() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
